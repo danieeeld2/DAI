036d9ddbfda164a69b29a9c7cb7e2094dd8c5c32
danieeeld2 <danieeeld2@gmail.com>
2023-10-20 10:53:40 +0200

    Base para la segunda parte

--- e-commerce/etienda/views.py
+++ e-commerce/etienda/views.py
@@ -6,33 +6,9 @@ from django.http import HttpResponse
 from . import models
 
 def index(request):
-    html = """
-    <html>
-    <header></header>
-    <body>
-        <h1>Consultas a la BD</h1>
-        <ul>
-            <li><a href="C1">Electrónica entre 100 y 200 euros, ordenados por precio</a></li>
-            <li><a href="C2">Productos que contengas la palabra pocket en la descripción</a></li>
-            <li><a href="C3">Productos con puntuación mayor de 4</a></li>
-            <li><a href="C4">Rompa de hombre, ordenada por puntuación</a></li>
-            <li><a href="C5">Facturación total</a></li>
-            <li><a href="C6">Facturación por categoria de producto</a></li>
-        </ul>
-    </body>
-    </html>
-    """
-    return HttpResponse(html)
+    context = {}
+    return render(request, 'etienda/index.html', context)
 
-def C1(request):
-    return HttpResponse(models.consulta1())
-def C2(request):
-    return HttpResponse(models.consulta2())
-def C3(request):
-    return HttpResponse(models.consulta3())
-def C4(request):
-    return HttpResponse(models.consulta4())
-def C5(request):
-    return HttpResponse(models.consulta5())
-def C6(request):
-    return HttpResponse(models.consulta6())
+def busqueda(request):
+    context = {'search_query' : request.GET.get('input_busqueda', '')}
+    return render(request, 'etienda/busqueda.html', context)
